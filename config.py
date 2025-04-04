20db0333616cbcd66212df7d4d22d0b86fb9322c
Amir-Jribi <amir.jribi.56@gmail.com>
2025-04-04 11:19:51 +0000

    adding hpl.py and config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+site_configuration = {
+    'systems': [
+        {
+            'name': 'my_cluster',  # Your cluster name
+            'descr': 'Cluster with Slurm scheduler',
+            'modules_system': 'lmod',
+            'hostnames': ['slurm-login-h23d5-u38-svn1.cloud.um6p.ma'],  # Login node hostname
+            'partitions': [
+                {
+                    'name': 'compute_hpl',
+                    'scheduler': 'slurm',
+                    'launcher': 'mpirun',  # Ensures jobs run on compute nodes
+                    'environs': ['intel'],
+                },
+            ]
+        }
+    ],
+    "environments": [
+     {
+            'name': 'intel',  # intel environment
+            'cc': 'icc',  # intel C compiler
+            'cxx': 'icpc',  # intel C++ compiler
+            'ftn': 'ifort',  # intel Fortran compiler
+            'modules' : ['intel-compilers/2022.2.0' , 'imkl/2021.3.0-gompi-2021a' , ' impi/2021.7.0-intel-compilers-2022.2.0'],
+    }
+  ]
+}
+
---
+++ hpl.py
@@ -0,0 +1,45 @@
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+@rfm.simple_test
+class HPLTest(rfm.RunOnlyRegressionTest):
+    descr="Reframe HPL Test on compute Node"
+    valid_systems=["my_cluster:compute_hpl"]
+    valid_prog_environs=["intel"]
+    #sourcesdir = '/home/amir.jribi-ext/use-reframe/hpl_test'
+    sourcesdir = '/home/amir.jribi-ext/benchmarks/hpl/mp_linpack'
+    reference={
+        'my_cluster:compute_hpl':{
+            'gflops_value':(2500,-0.2,0.2,'Gflops')
+        }
+    }
+    @run_before('run')
+    def setup_slurm(self):
+        self.num_tasks=1
+        self.num_tasks_per_node=1
+        self.num_cpus_per_task=56
+        self.time_limit = '10m30s'
+        self.exclusive_access = True
+        self.job.options += []
+
+
+    
+    #executable = '/home/amir.jribi-ext/benchmarks/hpl/mp_linpack/xhpl'
+    executable = './xhpl'
+    executable_opts = []
+    #executable = './stream_c.exe'
+    #@run_before('run')
+    #def set_slurm_partition(self):
+       # self.job.options += ['--nodes=1']
+
+
+    @sanity_function
+    def validate(self):
+        return sn.assert_found(r'PASSED', self.stdout)
+
+    @performance_function('Gflops')
+    def gflops_value(self):
+        return sn.extractsingle(r'Gflops\s+.*\nWC00C2R2\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\.\d+\s+([-+]?\d*\.?\d+(?:[eE][-+]?\d+)?)', self.stdout, 1, float)
+
+
+
