510434957bea00a77f52dd075b8903e35dd2a226
Amir-Jribi <amir.jribi.56@gmail.com>
2025-04-07 10:43:38 +0100

    modify the refrence range of stream

--- stream.py
+++ stream.py
@@ -14,7 +14,7 @@ class StreamTest(rfm.RegressionTest):
     valid_prog_environs=["intel_stream"]
     reference={
         'my_cluster:compute_stream':{
-            'triad_bw':(20000,-0.1,0.1,'MB/s')
+            'triad_bw':(22000,-0.2,0.2,'MB/s')
         }
     }
     num_threads = 56
